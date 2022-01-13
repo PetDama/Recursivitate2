b8b93e6bd98462e141a9a2d92be72b043986ba7f
Damaris Florina Petrila <damaris.petrila@emmanuel.ro>
2022-01-13 16:04:18 +0200

    Recursivitate2

---
+++ Recursivitate2/Recursivitate2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Recursivitate2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vector = new int[] { 5, 6, 7, 8, 9 };
+            Console.WriteLine(Recursivitate2.Maxim(vector, vector.Length -1, int.MinValue));
+        }
+    }
+}
---
+++ Recursivitate2/Recursivitate2/Recursivitate2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursivitate2
+{
+    class Recursivitate2
+    {
+
+        public static int Maxim(int[] vector, int n, int maxx)
+        {
+            if (n == 0)
+            {
+                if (maxx >= vector[n])
+                    return maxx;
+                else
+                    return vector[n];
+            }
+            else
+            {
+                if (maxx >= vector[n])
+                    return Maxim(vector, n - 1, maxx);
+                else
+                    return Maxim(vector, n - 1, vector[n]);
+            }
+        }
+    }
+}
+     
